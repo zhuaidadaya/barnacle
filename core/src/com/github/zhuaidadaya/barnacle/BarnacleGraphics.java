52d6009da2301a386c886673b3ab1be20dacdcb4
Zhuaidadaya <1501917367@qq.com>
2022-01-03 10:33:56 +0800

    ...

--- core/src/com/github/zhuaidadaya/barnacle/BarnacleGraphics.java
+++ core/src/com/github/zhuaidadaya/barnacle/BarnacleGraphics.java
@@ -12,7 +12,6 @@ public class BarnacleGraphics extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
